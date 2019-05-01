4c7aa5ae459682f402520c1a9dcb5c88f75ef732
Warren Reader <warrendreader@gmail.com>
2019-05-01 08:01:17 -0600

    Update understanding_typescript

--- understanding_typescript/app.js
+++ understanding_typescript/app.js
@@ -1,2 +1,52 @@
 "use strict";
-console.log("It works!");
+// string
+var myName = "Max";
+// myName = 29;
+//number
+var myAge = 27.5;
+// myAge = "Max";
+//boolean
+var hasHobbies = false;
+// hasHobbies = 1;
+// assign types
+var myRealAge;
+myRealAge = 27;
+// myRealAge = "27";
+// array
+var hobbies = ["Cooking", "Sports"];
+hobbies = [100];
+// hobbies = 100;
+// tuples
+var address = ["Superstreet", 99];
+// enum
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue"; // 2
+})(Color || (Color = {}));
+var myColor = Color.Green;
+console.log(myColor);
+// any
+var car = "BMW";
+console.log(car);
+car = {
+    brand: "BMW",
+    series: 3
+};
+console.log(car);
+// functions
+function returnMyName() {
+    return myName;
+}
+console.log(returnMyName());
+//void
+function sayHello() {
+    console.log("Hello!");
+}
+// argument types
+function multiply(value1, value2) {
+    return value1 * value2;
+}
+// console.log(multiply(2, "max"));
+console.log(multiply(10, 2));
--- understanding_typescript/app.ts
+++ understanding_typescript/app.ts
@@ -1,1 +1,63 @@
-console.log("It works!");
+// string
+let myName: string = "Max";
+// myName = 29;
+
+//number
+let myAge: number = 27.5;
+// myAge = "Max";
+
+//boolean
+let hasHobbies: boolean = false;
+// hasHobbies = 1;
+
+// assign types
+let myRealAge: number;
+myRealAge = 27;
+// myRealAge = "27";
+
+// array
+let hobbies: any[] = ["Cooking", "Sports"];
+hobbies = [100];
+// hobbies = 100;
+
+// tuples
+let address: [string, number] = ["Superstreet", 99];
+
+// enum
+enum Color {
+  Gray, // 0
+  Green, // 1
+  Blue // 2
+}
+
+let myColor: Color = Color.Green;
+console.log(myColor);
+
+// any
+let car: any = "BMW";
+console.log(car);
+car = {
+  brand: "BMW",
+  series: 3
+};
+console.log(car);
+
+// functions
+function returnMyName(): string {
+  return myName;
+}
+
+console.log(returnMyName());
+
+//void
+function sayHello(): void {
+  console.log("Hello!");
+}
+
+// argument types
+function multiply(value1: number, value2: number): number {
+  return value1 * value2;
+}
+
+// console.log(multiply(2, "max"));
+console.log(multiply(10, 2));
